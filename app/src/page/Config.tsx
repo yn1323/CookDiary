a9e0da2256f43d0c6221f0f184d25a365d04ef2c
yn1323 <yn1323@gmail.com>
2021-01-24 01:46:07 +0900

    fix: disable icon

--- app/src/page/Config.tsx
+++ app/src/page/Config.tsx
@@ -51,6 +51,7 @@ const Config = () => {
         <IconButton
           color="secondary"
           onClick={() => setDisableUserId(!disableUserId)}
+          disabled={!disableUserId}
         >
           <Edit />
         </IconButton>
