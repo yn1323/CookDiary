53d5fe7aef1d5e75d40cd86036e3a6b6a87c0550
yn1323 <yn1323@gmail.com>
2021-02-26 22:54:32 +0900

    feat: auth

--- app/src/page/Index.tsx
+++ app/src/page/Index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import Route from 'src/route'
 
 import { Box } from '@material-ui/core'
@@ -9,12 +9,24 @@ import NavigationHeader from 'src/component/template/NavigationHeader'
 import NavigationBottom from 'src/component/template/NavigationBottom'
 import LeftDrawer from 'src/component/template/LeftDrawer'
 import DialogTemplate from 'src/component/template/Dialog'
+import Loading from 'src/component/template/Loading'
+import { signIn } from 'src/helper'
 // import SpeedDialComponent from 'src/component/organism/SpeedDial'
 
 const Index = () => {
+  const [isSignIn, setIsSignIn] = useState(false)
+  useEffect(() => {
+    signIn({
+      successed: () => setIsSignIn(true),
+      failed: () => alert('Please Restart'),
+    })
+  }, [])
   // buildすると、なぜかRoute内にpyが付与されるので、分岐を追加
   // もしかするとエミュレータでも発生せず、apk化したときのみ発生する事象の回避用かも
   const padding = process.env.NODE_ENV === 'production' ? 7 : 7
+  if (!isSignIn) {
+    return <Loading />
+  }
   return (
     <>
       {/* <NavigationTop /> */}
